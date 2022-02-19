5eac5e72abc5b5721b37ed66daaf33d562704bb5
rizgust <rizgust.dev@gmail.com>
2022-02-19 11:28:49 +0700

    [ADD] db config using url

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-52-87-134-4.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'dcemrkhhch6k4b'),
-      user: env('DATABASE_USERNAME', 'mrwazmkhairusq'),
-      password: env('DATABASE_PASSWORD', 'cbaf73537874c531ef87f898480b1e6b3099c8c7882dc043c659efd61ffc3e8c'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
